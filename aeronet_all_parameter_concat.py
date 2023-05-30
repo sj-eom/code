5402f017393eeb86eba35e8a9a65d6c3fbe1a18a
sj-eom <121787538+sj-eom@users.noreply.github.com>
2023-05-30 15:44:56 +0900

    Update aeronet_all_parameter_concat.py

--- aeronet_all_parameter_concat.py
+++ aeronet_all_parameter_concat.py
@@ -1,6 +1,15 @@
-# path = 'C:/Users/PC/OneDrive - UNIST/SPARTAN/aeronet_seoul/20190101_20221231_Seoul_SNU' #서울대 자료
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime, timedelta
+import math
+import glob
+from matplotlib.colors import ListedColormap
+from mpl_toolkits.mplot3d import Axes3D
+#%%
+path = 'C:/Users/PC/OneDrive - UNIST/SPARTAN/aeronet_seoul/20190101_20221231_Seoul_SNU' #서울대 자료
 # path = 'C:/Users/PC/OneDrive - UNIST/SPARTAN/aeronet_yonsei/20190101_20221231_Yonsei_University' #연세대 자료
-path = 'C:/Users/PC/OneDrive - UNIST/SPARTAN/aeronet_unist/20200101_20221231_KORUS_UNIST_Ulsan' #유니스트 자료
+# path = 'C:/Users/PC/OneDrive - UNIST/SPARTAN/aeronet_unist/20200101_20221231_KORUS_UNIST_Ulsan' #유니스트 자료
 
 data_aod = path+'.lev15'
 data_ssa = path+'.ssa'
@@ -28,12 +37,12 @@ for j in range (0, np.size(parameter)):
     globals()['data_'+str(parameter[j])] = globals()['data_'+str(parameter[j])].set_index('times')
     
 #%% ARRANGE
-data_AOD = data_aod[['AOD_440nm','AOD_500nm','AOD_675nm','AOD_870nm','AOD_1020nm']] 
+data_AOD = data_aod[['AOD_440nm','AOD_500nm','AOD_675nm','AOD_870nm','AOD_1020nm','Precipitable_Water(cm)','440-870_Angstrom_Exponent']] 
 data_FMF = data_fmf[['FineModeFraction_500nm[eta]','Total_AOD_500nm[tau_a]','Fine_Mode_AOD_500nm[tau_f]']] #to compare with 550 and 500
 data_SSA = data_ssa[['Single_Scattering_Albedo[440nm]','Single_Scattering_Albedo[675nm]','Single_Scattering_Albedo[870nm]','Single_Scattering_Albedo[1020nm]']]
 data_AAE = data_aae[['Absorption_AOD[440nm]','Absorption_AOD[870nm]','Absorption_Angstrom_Exponent_440-870nm']]
 data_EAE = data_eae[['AOD_Extinction-Total[440nm]','AOD_Extinction-Total[870nm]','Extinction_Angstrom_Exponent_440-870nm-Total']]
-data_DEP = data_dep[['Lidar_Ratio[440nm]','Depolarization_Ratio[440nm]']]
+data_DEP = data_dep[['Lidar_Ratio[440nm]','Depolarization_Ratio[1020nm]']]
 data_SIZ = data_siz[data_siz.columns[5:27]]
 ############reset index to merge#################
 parameter_c = ['AOD', 'FMF', 'SSA', 'AAE', 'EAE', 'DEP', 'SIZ']
@@ -82,5 +91,4 @@ data = data[['AOD_440nm','AOD_550','FMF_550','SSA_440']]
 aero = data_ae.copy()
 siz = data_ae.copy()
 siz = siz[siz.columns[20:42]]
-# siz.to_csv('C:/Users/PC/OneDrive - UNIST/SPARTAN/aeronet_seoul/seoul_aeronet+siz.csv')
-# aero.to_csv(path+'_full_parameter.csv')
+# aero.to_csv(path+'_new_parameter.csv')
