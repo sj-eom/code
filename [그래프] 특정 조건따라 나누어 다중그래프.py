13dc40db68fe4698488609cecdd39a4802700d0d
sj-eom <121787538+sj-eom@users.noreply.github.com>
2023-07-31 15:49:45 +0900

    Update [그래프] 특정 조건따라 나누어 다중그래프.py

--- [그래프] 특정 조건따라 나누어 다중그래프.py
+++ [그래프] 특정 조건따라 나누어 다중그래프.py
@@ -33,3 +33,34 @@ for j in range (0, np.size(f.index)):
         continue
 
 
+또는, 
+
+#%% 그룹별로 나뉜 자료를 샘플링 기간에 맟줘 평균한 상관계수 #############################################################################
+fig10, ax10 = plt.subplots(2,2, figsize = (16,16))
+# 격자 여백 설정
+plt.subplots_adjust(wspace = 0.5, hspace = 0.5)
+colors = ['darkviolet','red','blue','white']
+for c in colors:                            #컬러에 따른 그림 위치 정해준다
+    if c == 'darkviolet':
+        a, b = 0, 0   
+    if c == 'red':
+        a, b = 0, 1  
+    if c == 'blue':
+        a, b = 1, 0  
+    if c == 'white':
+        a, b = 1, 1  
+    
+    #나눠진 색깔 그룹을 다시 같은 샘플링 기간끼리 평균한다
+    f_each_group = f[f['color']==c].groupby('Filter PM2.5 mass').mean() 
+    #상관관계 계산
+    f_ssa_list = f_each_group['SSA_1020']
+    f_ratio_list = f_each_group['Ammoniated Sulfate']/f_each_group.index
+    f_corr = pd.DataFrame([f_ssa_list, f_ratio_list]).T
+    f_corr = f_corr.dropna(axis=0)
+    #스캐터플롯 및 상관관계 같이 그리기
+    ax10[a,b].scatter(f_each_group['SSA_1020'], f_each_group['Ammoniated Sulfate']/f_each_group.index, color=c, edgecolor='grey', s=150)
+    ax10[a,b].text(0.86,0.9,'R='+str(round(np.ma.corrcoef(f_corr['SSA_1020'], f_corr[f_corr.columns[1]])[0,1],3)), fontsize=30)
+    ax10[a,b].set_xlim(0.8,1) 
+    ax10[a,b].set_ylim(0,1)
+    ax10[a,b].set_xlabel('SSA 1020', fontsize=20) 
+    ax10[a,b].set_ylabel('sulfate ratio', fontsize=20)
